2bf25a8a6f9f8cb72ad991a0ab91c5be60f03a4d
Kuda <kudazata@gmail.com>
2022-06-24 07:59:41 +0200

    Initial Commit

---
+++ StreamingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StreamingApp
+//
+//  Created by Kuda Zata on 24/6/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
